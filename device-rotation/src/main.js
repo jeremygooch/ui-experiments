2c270e4ce3bdf157c1bc003c4438c10816bf629c
Jeremy Gooch <jeremy.gooch@gmail.com>
2017-11-30 18:48:44 -0600

    Updating elements and adjusting beta.

--- device-rotation/src/main.js
+++ device-rotation/src/main.js
@@ -3,26 +3,26 @@
 require('./main.scss');
 
 class maintainPerspective {
-    constructor([c, s, o]) {
-        this.container = document.querySelector(c);
-        this.stage     = document.querySelector(s);
-        this.output    = document.querySelector(o);
+    constructor({container, stage} = {}) {
+        this.container = document.querySelector(container);
+        this.stage     = document.querySelector(stage);
     }
 
     getOptions() {
         return {
             maxX: this.stage.clientWidth  - this.container.clientWidth,
-            maxY: this.stage.clientHeight - this.container.clientHeight
+            maxY: this.stage.clientHeight - this.container.clientHeight,
+            containerWidth: this.container.clientWidth
         }
     }
 
     modifyOrientation(event) {
         let options = this.getOptions();
-        let x = event.beta;  // In degree in the range [-180,180]
+        let x = event.beta - 61;  // In degree in the range [-180,180]
         let y = event.gamma; // In degree in the range [-90,90]
 
-        this.output.innerHTML  = "beta : " + x + "\n";
-        this.output.innerHTML += "gamma: " + y + "\n";
+        console.log("beta : " + x + "\n");
+        console.log("gamma: " + y + "\n");
 
         // Because we don't want to have the device upside down
         // We constrain the x value to the range [-90,90]
@@ -36,10 +36,18 @@ class maintainPerspective {
 
         // 10 is half the size of the container
         // It center the positioning point to the center of the container
-        this.container.style.top  = (options.maxX*x/180 - 10) + "px";
-        this.container.style.left = (options.maxY*y/180 - 10) + "px";
+        this.container.style.top  = (options.maxX*x/180 - (options.containerWidth/2)) + "px";
+        this.container.style.left = (options.maxY*y/180 - (options.containerWidth/2)) + "px";
     }
 }
 
-var p = new maintainPerspective(['.container', '.stage', '.output']);
+// var p = new maintainPerspective(['.container', '.stage']);
+var p = new maintainPerspective({container: '.container', stage: '.stage'});
 window.addEventListener('deviceorientation', () => p.modifyOrientation(event));
+
+
+var newDiv = document.createElement("div");
+var newContent = document.createTextNode("Hi there and greetings!");
+newDiv.appendChild(newContent);
+var currentDiv = document.querySelector(".stage");
+currentDiv.appendChild(newDiv);
