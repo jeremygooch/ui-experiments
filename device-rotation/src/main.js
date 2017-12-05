2658ddfa2a5a93bbea2fa8fe498624efb287bdd7
Jeremy Gooch <jeremy.gooch@gmail.com>
2017-12-04 20:20:57 -0600

    Inverting and adjusting rotation values to keep box closer to user.

--- device-rotation/src/main.js
+++ device-rotation/src/main.js
@@ -32,28 +32,19 @@ class FollowDeviceRotation extends LayoutHelper {
         return {
             maxX: this.stage.clientWidth  - this.container.clientWidth,
             maxY: this.stage.clientHeight - this.container.clientHeight,
-            containerWidth: this.container.clientWidth
+            anchor: this.container.clientWidth / 2
         };
-    }
-
-    calcDropshadowDrift(area, gamma, perDrift, containerWidth) {
-        // return area*gamma/177 + (area*perDrift/177 > 0 ? area*perDrift/177 : area*perDrift/177 * -1) - (containerWidth/2);
     }
 
     update(event) {
         let options = this.getOptions();
-        let x = event.beta - 61;  // In degree in the range [-180,180]
-        let y = event.gamma; // In degree in the range [-90,90]
+        let x = event.beta - 90;  // In degree in the range [-180,180]
+        let y = event.gamma - 90; // In degree in the range [-90,90]
         let dx = 0;
         let dy = 0;
 
         // console.log("beta : " + x + "\n");
         // console.log("gamma: " + y + "\n");
-
-        // Because we don't want to have the device upside down
-        // We constrain the x value to the range [-90,90]
-        if (x >  90) { x =  90};
-        if (x < -90) { x = -90};
 
         // To make computation easier we shift the range of 
         // x and y to [0,180]
@@ -62,25 +53,26 @@ class FollowDeviceRotation extends LayoutHelper {
         dx = LayoutHelper.calcPercent(5, x);
         dy = LayoutHelper.calcPercent(5, y);
 
-        console.log(dx);
+        // var anchor = options.containerWidth/2;
+        var scrRotate = (axis = 'x') => { return axis == 'x' ? options.maxX*x/180 : options.maxY*y/180; };
 
-        // 10 is half the size of the container
+        function forcePos(val) { return val < 0 ? 0 : val}
+
         // It center the positioning point to the center of the container
-        this.container.style.top  = (options.maxX*x/180 - (options.containerWidth/2)) + "px";
-        this.container.style.left = (options.maxY*y/180 - (options.containerWidth/2)) + "px";
+        this.container.style.top  = forcePos((-1 * (scrRotate('x') - options.anchor))) + "px";
+        this.container.style.left = forcePos((-1 * (scrRotate('y') - options.anchor))) + "px";
 
-        this.dropshadow.style.top  = (options.maxX*x/177 + options.maxX*dx/177 - (options.containerWidth/2)) + "px";
+        this.dropshadow.style.top  = forcePos((-1 * (scrRotate('x') + options.maxX*dx/180 - options.anchor))) + "px";
 
-        this.dropshadow.style.left = (options.maxY*y/177 + (options.maxY*dy/177 > 0 ? options.maxY*dy/177 : options.maxY*dy/177 * -1) - (options.containerWidth/2)) + "px";
+        this.dropshadow.style.left = forcePos(-1 * (scrRotate('y') + (options.maxY*dy/180 > 0 ? options.maxY*dy/180 : options.maxY*dy/180 * -1) - options.anchor)) + "px";
 
 
-        
-        // Special Effects
+
+        // Special Effects (simulate depth of field)
         this.dropshadow.style.filter = 'blur(' + (2 + ((dx + dy) / 2)) + 'px)';
         this.dropshadow.style.opacity = 0.75 / ((dx + dy) / 2);
     }
 }
 
-// var p = new maintainPerspective(['.container', '.stage']);
 var f = new FollowDeviceRotation({container: '.container', stage: '.stage', dropshadow: '.dropshadow'});
 window.addEventListener('deviceorientation', () => f.update(event));
